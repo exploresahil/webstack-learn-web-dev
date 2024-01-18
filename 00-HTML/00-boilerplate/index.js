bf0bab79a6c12e14c70d3542dd735c9dce8ac6ec
Sahil Satpute <explore.sahil1@gmail.com>
2024-01-18 15:50:14 +0530

    updated html

---
+++ 00-HTML/00-boilerplate/index.js
@@ -0,0 +1,9 @@
+const main = document.createElement("main");
+
+document.body.appendChild(main);
+
+const mainElement = document.querySelector("main");
+
+mainElement.style.width = "200px";
+mainElement.style.height = "200px";
+mainElement.style.backgroundColor = "orange";
