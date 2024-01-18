d760ea57e6e23e2562c5af96e2a4b37546c54bfb
Sahil Satpute <explore.sahil1@gmail.com>
2024-01-18 15:54:18 +0530

    Update index.js

--- 00-HTML/00-boilerplate/index.js
+++ 00-HTML/00-boilerplate/index.js
@@ -1,9 +1,20 @@
+// Creating the main element
 const main = document.createElement("main");
 
+// Adding the main the body element
 document.body.appendChild(main);
 
+// Selecting the main element
 const mainElement = document.querySelector("main");
 
+// Set the class and id
+mainElement.className = "main-container";
+mainElement.id = "main";
+
+// Set the CSS properties
 mainElement.style.width = "200px";
 mainElement.style.height = "200px";
 mainElement.style.backgroundColor = "orange";
+
+// Displaying the main element in the console
+console.log("main ->", mainElement);
